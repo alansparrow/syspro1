5413d8697b0e9da4054401250b072c128f826c27
Alan Sparrow <trannguyenbaotrung@gmail.com>
2013-09-18 16:32:14 +0000

    first commit

---
+++ makefile/file1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void func1() {
+  printf("function 1 \n");
+}
+
+void func2() {
+  printf("function 2\n");
+}
---
+++ temp.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+int main(void)
+{
+  _exit(0);
+}
